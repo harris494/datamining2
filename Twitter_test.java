6fdb6632c7a94c13b90b39672207ce28c13b9bdc
harris494 <harris1994sun@gmail.com>
2016-10-18 15:23:33 -0400

    Add files via upload

---
+++ Twitter_test.java
@@ -0,0 +1,53 @@
+package api;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+/**
+ * Implementation Class
+ * Take out Time and Text from whole twitter file
+ * @author Harris
+ *
+ */
+public class Twitter_test {
+
+public static void main(String[] args) throws IOException  {
+// TODO Auto-generated method stub
+	File file = new File("D:/txt/e.json");
+	String JsonContext = new Twitter_util().ReadFile("D:\\workspace_python\\music_1.json");
+	JSONArray jsonArray = JSONArray.fromObject(JsonContext);
+	int size = jsonArray.size();
+	System.out.println("Size: " + size);
+	BufferedWriter bw = null;
+	String t ="";
+		for(int  i = 0; i < size; i++){
+			bw = new BufferedWriter(new FileWriter(file,true));
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+			String temp1 = "{'Time':"+ jsonObject.get("created_at")+",";
+			String temp2 = "'Text':" + jsonObject.get("text")+"},";
+
+			bw.write(temp1);
+//			bw.newLine();
+			bw.append(temp2);
+			bw.newLine();
+			bw.flush();
+			
+			}
+
+		bw.close();
+
+//System.out.println("[" + i + "]Time="+ jsonObject.get("created_at"));
+//System.out.println("[" + i + "]text=" + jsonObject.get("text"));
+}
+}
---
+++ Twitter_util.java
@@ -0,0 +1,45 @@
+package api;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+/**
+ * read in local file
+ * @author Harris
+ *
+ */
+public class Twitter_util {
+
+public String ReadFile(String Path){
+	BufferedReader reader = null;
+	String laststr = "";
+	try{
+		FileInputStream fileInputStream = new FileInputStream(Path);
+		InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+		reader = new BufferedReader(inputStreamReader);
+		String tempString = null;
+		while((tempString = reader.readLine()) != null){
+			laststr += tempString;	
+		}
+		reader.close();
+	}catch(IOException e){
+		e.printStackTrace();
+	}finally{
+		if(reader != null){
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	return laststr;
+	}
+
+}
